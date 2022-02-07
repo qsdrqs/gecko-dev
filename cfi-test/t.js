b967d1a2f7598c4ee6f2189feba5ed53f6512f7a
Zijie Zhao <zzjas98@gmail.com>
2022-02-07 02:46:20 -0800

    output spew

--- cfi-test/t.js
+++ cfi-test/t.js
@@ -2,9 +2,10 @@ function f1(a, b) {
 	return 12345+a+b+arguments.length;
 }
 function f2() {
-	for (let i = 0; i < 10; i++) {
+	for (let i = 0; i < 20; i++) {
 		f1(i, i+1);
 	}
 }
 f2()
+print(dis(f1))
 print(disnative(f1))
